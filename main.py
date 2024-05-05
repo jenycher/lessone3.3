172892106763ef52ae8dc450a01d72e4ab12704a
jenycher <168509289+jenycher@users.noreply.github.com>
2024-05-05 19:24:50 +0300

    add score

--- main.py
+++ main.py
@@ -8,29 +8,40 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon=pygame.image.load("img/tir.jpg")
+icon = pygame.image.load("img/tir.jpg")
 pygame.display.set_icon(icon)
 
-target_img = icon=pygame.image.load("img/tar.png")
+target_img = icon = pygame.image.load("img/tar.png")
 target_width = 80
 target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 running = True
+score = 0  # Переменная для хранения счета
+
+font = pygame.font.SysFont(None, 36)  # Шрифт для отображения счета
+
 while running:
-      screen.fill(color)
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              running = False
-          if event.type == pygame.MOUSEBUTTONDOWN:
-              mouse_x, mouse_y = pygame.mouse.get_pos()
-              if target_x< mouse_x <target_x + target_width and target_y< mouse_y <target_y + target_height:
-                  target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                  target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-      screen.blit(target_img, (target_x, target_y))
-      pygame.display.update()
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1  # Увеличиваем счет на 1 при попадании
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+
+    # Отображаем счет на экране
+    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(score_text, (SCREEN_WIDTH - 150, 10))
+
+    pygame.display.update()
 
 pygame.quit()
