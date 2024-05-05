63770886b10a76d5232354326f6b07ad6b7940e2
jenycher <168509289+jenycher@users.noreply.github.com>
2024-05-05 19:05:35 +0300

    upd cycle

--- main.py
+++ main.py
@@ -18,9 +18,18 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255)).
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 running = True
 while running:
-    pass
-
+      screen.fill(color)
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              running = False
+          if event.type == pygame.MOUSEBUTTONDOWN:
+              mouse_x,mouse_y=pygame.mouse.get.pos()
+              if target_x< mouse_x <target_x + target_width and target_y< mouse_y <target_y + target_height:
+                  target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                  target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                  pygame.display.update()
+                  screen.blit(target_img, (target_x, target_y))
 pygame.quit()
